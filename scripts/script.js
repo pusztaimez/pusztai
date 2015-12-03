c7a4408d1f62bcf00bb060074757deaeb2dc8196
mantecol <czedli@gmail.com>
2015-12-03 17:24:10 +0100

    navigáció + item paddingok

--- scripts/script.js
+++ scripts/script.js
@@ -9,3 +9,21 @@ menuGomb.addEventListener('click', menuNyitoCsuko);
 if (window.innerWidth < 500) {
 menuNyitoCsuko();
 }
+
+$('#nav').onePageNav({
+    currentClass: 'current',
+    changeHash: false,
+    scrollSpeed: 750,
+    scrollThreshold: 0.5,
+    filter: '',
+    easing: 'swing',
+    begin: function() {
+        //I get fired when the animation is starting
+    },
+    end: function() {
+        //I get fired when the animation is ending
+    },
+    scrollChange: function($currentListItem) {
+        //I get fired when you enter a section and I pass the list item of the section
+    }
+});
