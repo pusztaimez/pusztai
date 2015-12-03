822671824c623240003d83ec4b6f64efc896296e
vargaevi <vargaevi@gmail.com>
2015-12-03 15:03:59 +0100

    hamburger

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,11 @@
+
 var menu = document.getElementById('menuContainer');
 var menuGomb = document.getElementById('menuToggle');
-
 function menuNyitoCsuko () {
   menu.classList.toggle('hide');
 }
 
 menuGomb.addEventListener('click', menuNyitoCsuko);
-
 if (window.innerWidth < 500) {
-  menuNyitoCsuko();
+menuNyitoCsuko();
 }
-
-function scrollKiIro () {
-  console.log(document.body.scrollTop);
-}
-
-window.addEventListener('scroll', scrollKiIro)
